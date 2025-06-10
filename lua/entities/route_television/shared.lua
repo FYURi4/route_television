1de8bc3deb164ffebc800aae532e40458abe4c67
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-06-10 18:31:01 +0300

    Create shared.lua

---
+++ lua/entities/route_television/shared.lua
@@ -0,0 +1,21 @@
+ENT.Base = "base_gmodentity"
+
+ENT.Type = "anim"
+
+ENT.Category = "3DEMC_ENT"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "Route_Television"
+
+ENT.PrintName = "Route_Television"
+
+ENT.Author = "FURA"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = ""
+
+ENT.Instructions = ""
